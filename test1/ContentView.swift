1d202e6d69e755eb446739fe50375445e8f5c21d
minji choi <so8376@gmail.com>
2022-07-23 05:00:37 +0900

    init

---
+++ test1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by 최민지 on 2022/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by 최민지 on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
