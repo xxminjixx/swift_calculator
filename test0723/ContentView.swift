6247b6b51283553c28fc271c7bd9fdb90d6027ee
minji choi <so8376@gmail.com>
2022-07-24 18:28:04 +0900

    계산기 버튼 배치

--- test0723/ContentView.swift
+++ test0723/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isShowing = false
+    
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
     var body: some View {
-        Button("Alert 띄우기"){
-            isShowing = true
-        }
-        .alert("메시지", isPresented: $isShowing){
-            Button("OK", role: .cancel) {}
-        }
+        
+        ZStack(alignment: .bottom){
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 12){
+                
+                HStack{
+                    Spacer()
+                    Text("42").foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding() //end of HStack
+                
+                ForEach(buttons, id: \.self){ row in
+                    HStack(spacing: 12){
+                        ForEach(row, id: \.self){ button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }//end of HStack
+                }
+            }.padding(.bottom) //end of VStack
+        }//end of ZStack
+    }
+    
+    func buttonWidth() -> CGFloat{
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
